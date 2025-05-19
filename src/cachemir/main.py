9278148dfe160dd0d7c5c4033ab9ef516a3e815e
Mateusz Lacki <matteo.lacki@gmail.com>
2025-05-19 19:22:45 +0200

    dropping duplicates before evals

--- src/cachemir/main.py
+++ src/cachemir/main.py
@@ -79,6 +79,88 @@ def input_to_bytes(
 
 
 # TODO: perhaps add this to the Prosit model instance.
+# def get_index_and_stats(
+#     path: Path | str,
+#     inputs_df: pd.DataFrame,
+#     results_iter: Callable[[tuple[Iterable, ...]], Iterator[MemoizedOutput]],
+#     input_types: dict[str, type],
+#     stats_types: dict[str, type],
+#     verbose: bool = False,
+# ) -> tuple[pd.DataFrame, pd.DataFrame]:
+#     """ """
+#     path = Path(path)
+#     path.mkdir(parents=True, exist_ok=True)
+
+#     in2bytes = functools.partial(
+#         input_to_bytes,
+#         types=tuple(input_types.values()),
+#     )
+#     out2bytes = functools.partial(
+#         input_to_bytes,
+#         types=(int, int, *tuple(stats_types.values())),
+#     )
+
+#     with (
+#         Index(path / "index.lmbd").open("w") as txn,
+#         DatasetWriter(path / "data", append_ok=True) as mmapped_df,
+#     ):
+#         inputs = inputs_df.itertuples(index=False, name=None)
+#         if verbose:
+#             inputs = tqdm(
+#                 inputs,
+#                 total=len(inputs_df),
+#                 desc="Establishing what is cached",
+#             )
+#         outputs = []
+#         for _in in inputs:
+#             value = txn.get(in2bytes(_in), None)
+#             if value is not None:
+#                 value = tuple(msgpack.unpackb(value))
+#             outputs.append(value)
+
+#         missing_idxs = [i for i, o in enumerate(outputs) if o is None]
+#         missing_df = inputs_df.iloc[missing_idxs]
+
+#         if len(missing_idxs) == 0:
+#             if verbose:
+#                 print("All calls were in cache.")
+#         else:
+#             if verbose:
+#                 print(f"{len(missing_idxs)} calls were not in cache.")
+#             idx = txn.stat()["entries"]  # current maximal idx
+#             missing_results = results_iter(
+#                 **{col: missing_df[col].to_numpy() for col in missing_df},
+#             )
+#             if verbose:
+#                 missing_results = tqdm(
+#                     missing_results,
+#                     total=len(missing_idxs),
+#                     desc="Saving missing results",
+#                 )
+
+#             for missing_idx, missing_result in zip(missing_idxs, missing_results):
+#                 out_stats = (idx, len(missing_result.data), *missing_result.stats)
+#                 txn.put(
+#                     in2bytes(missing_result.input),
+#                     out2bytes(out_stats),
+#                 )
+#                 mmapped_df.append_df(missing_result.data)
+#                 idx += len(missing_result.data)
+#                 assert outputs[missing_idx] is None
+#                 outputs[missing_idx] = out_stats
+
+# index_and_stats = pd.DataFrame(
+#     outputs, copy=False, columns=["idx", "cnt", *list(stats_types)]
+# )
+# raw_data = open_dataset(path / "data")
+
+# return index_and_stats, raw_data
+
+
+def ITERTUPLES(df):
+    yield from df.itertuples(index=False, name=None)
+
+
 def get_index_and_stats(
     path: Path | str,
     inputs_df: pd.DataFrame,
@@ -99,45 +181,23 @@ def get_index_and_stats(
         input_to_bytes,
         types=(int, int, *tuple(stats_types.values())),
     )
-
     with (
         Index(path / "index.lmbd").open("w") as txn,
         DatasetWriter(path / "data", append_ok=True) as mmapped_df,
     ):
-        inputs = inputs_df.itertuples(index=False, name=None)
-        if verbose:
-            inputs = tqdm(
-                inputs,
-                total=len(inputs_df),
-                desc="Establishing what is cached",
-            )
-        outputs = []
-        for _in in inputs:
-            value = txn.get(in2bytes(_in), None)
-            if value is not None:
-                value = tuple(msgpack.unpackb(value))
-            outputs.append(value)
+        missing_idxs = [
+            i
+            for i, row in enumerate(ITERTUPLES(inputs_df))
+            if txn.get(in2bytes(row), None) is None
+        ]
+        missing_df = inputs_df[list(input_types)].iloc[missing_idxs]
 
-        missing_idxs = [i for i, o in enumerate(outputs) if o is None]
-        missing_df = inputs_df.iloc[missing_idxs]
-
-        if len(missing_idxs) == 0:
-            if verbose:
-                print("All calls were in cache.")
-        else:
-            if verbose:
-                print(f"{len(missing_idxs)} calls were not in cache.")
+        if len(missing_df) > 0:
+            missing_df = missing_df.drop_duplicates()
             idx = txn.stat()["entries"]  # current maximal idx
             missing_results = results_iter(
                 **{col: missing_df[col].to_numpy() for col in missing_df},
             )
-            if verbose:
-                missing_results = tqdm(
-                    missing_results,
-                    total=len(missing_idxs),
-                    desc="Saving missing results",
-                )
-
             for missing_idx, missing_result in zip(missing_idxs, missing_results):
                 out_stats = (idx, len(missing_result.data), *missing_result.stats)
                 txn.put(
@@ -146,8 +206,13 @@ def get_index_and_stats(
                 )
                 mmapped_df.append_df(missing_result.data)
                 idx += len(missing_result.data)
-                assert outputs[missing_idx] is None
-                outputs[missing_idx] = out_stats
+
+    with Index(path / "index.lmbd").open("r") as txn:
+        outputs = []
+        for _in in ITERTUPLES(inputs_df):
+            value = txn.get(in2bytes(_in), None)
+            assert value is not None
+            outputs.append(tuple(msgpack.unpackb(value)))
 
     index_and_stats = pd.DataFrame(
         outputs, copy=False, columns=["idx", "cnt", *list(stats_types)]
